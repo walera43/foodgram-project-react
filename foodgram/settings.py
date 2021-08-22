18f1abac385e43c354335555cda7a4c22016c802
Валерий Изгаршев <Walera47@gmail.com>
2021-08-23 01:07:51 +0300

    Fixes PEP8

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,7 +90,6 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 
 # Password validation
